6e72b1d70740b15e5a33117310c18e4b9abac062
robot <NotaRobot@erafn.xyz>
2022-07-17 18:13:41 +0100

    only pushing this to test on my hack

--- KextUpdater/Program.cs
+++ KextUpdater/Program.cs
@@ -124,6 +124,8 @@ foreach (string curdir in dirs)
                             Directory.Delete(kextdir + @"\" + dir.Name, true);
                         // Directory.Move(dir.FullName, kextdir + @"\" + dir.Name); // this causes #1, workaround is to use visual basic func FileSystem.CopyDirectory();
                         FileSystem.CopyDirectory(dir.FullName, kextdir + @"\" + dir.Name);
+                        Console.WriteLine(kextdir + @"\" + dir.Name);
+                        Console.WriteLine(kextdir + dir.Name);
                         Directory.Delete(dir.FullName, true);
                         if (Directory.Exists(tmp + @"\" + downloadName))
                         Directory.Delete(tmp + @"\" + downloadName, true);
